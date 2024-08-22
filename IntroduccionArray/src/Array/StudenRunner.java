459a013476561bd11f6a394cab446ee6ba3e45ed
JARAMILLO <JARAMILLO@DESKTOP-DHSAPM8>
2024-08-21 22:34:23 -0500

    Iniciando Arrays

---
+++ IntroduccionArray/src/Array/StudenRunner.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class StudenRunner {
+
+	public static void main(String[] args) {
+		int[] marks = { 99, 98, 100 };
+
+		Student student = new Student("Ranga", marks);
+	}
+}
---
+++ IntroduccionArray/src/Array/Student.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Student {
+
+	private String name;
+	private int[] marks;
+
+	public Student(String name, int[] marks) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.marks = marks;
+	}
+
+}
